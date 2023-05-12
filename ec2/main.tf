b4a63bd928338d07b19a8a425c14b84052afa4f6
krish <krishnaja-b>
2023-05-12 21:38:39 +0530

    roboshop-infra

--- ec2/main.tf
+++ ec2/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "ec2" {
 
 }
 resource "null_resource" "provisioner" {
-  provisioner "remote-exec" {
+  provisioner "remote-exec"{
     connection {
       host = aws_instance.ec2.public_ip
       user = "centos"
