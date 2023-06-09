0b94a3289672971e9af9a9ed940ddf55b31c09a3
krish <krishnaja-b>
2023-06-09 09:33:05 +0530

    vars,data,main(ec2)

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,9 +1,3 @@
-data "aws_caller_identity" "current" {}
-data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "devops-practice-with-ansible"
-  owners           = [data.aws_caller_identity.current.account_id]
-}
 
 resource "aws_instance" "ec2" {
   ami = data.aws_ami.ami.image_id
@@ -59,11 +53,6 @@ resource "aws_security_group" "sg" {
 
 }
 
-variable "component" {}
-variable "instance_type" {}
-variable "env" {
-  default = "dev"
-}
 
 
 
