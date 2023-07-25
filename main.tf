b43063b9cf43bb1e5cee5fbb0dd1db168d33844e
krish <krishnaja-b>
2023-07-25 23:15:51 +0530

    solvong tags error

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
   source   = "git::https://github.com/krishnaja-b/tf-module-vpc.git"
   env = var.env
-  tags = var.tags
   for_each = var.vpc
   vpc_cidr     = each.value["vpc_cidr"]
 }
+
 
 
 
