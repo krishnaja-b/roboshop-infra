1bc95e599d487591c0f2b74aee59a2e8a03d523c
krish <krishnaja-b>
2023-07-28 09:58:15 +0530

    error correction

--- main.tf
+++ main.tf
@@ -3,8 +3,6 @@ module "vpc" {
   env = var.env
   for_each = var.vpc
   vpc_cidr     = each.value["vpc_cidr"]
-  public_subnets = each.value["public_subnets"]
-  private_subnets = each.value["private_subnets"]
 }
 
 
