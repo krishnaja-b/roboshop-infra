ec95f56e2a55aa479b1882e5cd28e821a9162670
krish <krishnaja-b>
2023-08-03 12:28:06 +0530

    declaring default vpc,route variables

--- env-prod/terraform.tfvars
+++ env-prod/terraform.tfvars
@@ -5,4 +5,5 @@
   cost_center  = 1001
 }
 
-
+default_vpc_id = "vpc-042992ad68653bd97"
+default_route_table= "rtb-123"
--- vars.tf
+++ vars.tf
@@ -3,6 +3,8 @@ variable "env" {}
 variable "tags" {}
 variable "public_subnets" {}
 variable "private_subnets" {}
+variable "default_vpc_id" {}
+variable "default_route_table" {}
 
 
 
