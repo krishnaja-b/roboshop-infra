8004b9969454c50023775a98316e0bd43b020f35
krish <krishnaja-b>
2023-08-18 22:15:40 +0530

    creating autoscaling group

--- main.tf
+++ main.tf
@@ -76,13 +76,17 @@ load_balancer_type = each.value["load_balancer_type"
 subnets = lookup(local.subnet_ids,each.value["subnet_name"], null)
 }
 
-module "apps" {
-source = "git::https://github.com/krishnaja-b/tf-module-apps.git"
+module "app" {
+source = "git::https://github.com/krishnaja-b/tf-module-app.git"
 env = var.env
 tags = var.tags
 for_each = var.apps
 component = each.value["component"]
 instance_type = each.value["instance_type"]
+desired_capacity = each.value["desired_capacity"]
+max_size = each.value["max_size"]
+min_size = each.value["min_size"]
+subnets = lookup(local.subnet_ids,each.value["subnet_name"], null)
 
 }
 
